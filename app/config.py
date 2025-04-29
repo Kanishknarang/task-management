d217387e699e9fdd0b875ef418481c153cf08386
Kanishk Narang <kanishknarang@Kanishks-MacBook-Air.local>
2025-04-29 23:16:21 +0530

    adding migration support

--- app/config.py
+++ app/config.py
@@ -3,7 +3,8 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     DATABASE_URL: str
 
+
     class Config:
-        env_file = ".env"
+        env_file = ".env.app"
 
 settings = Settings()
